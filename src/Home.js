bbb2c28b08f549a3e37d00ab224fb95bb9220d81
Ryan Bartsch <rbartsch@adam.com.au>
2020-04-27 21:51:44 +0930

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Homex</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
